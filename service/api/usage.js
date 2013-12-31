8cb1d526c80e146712e8fd9c09a790b375075984
windowsazure <windowsazure>
2013-12-31 15:58:17 +0000

    Committing update from request /scmvfs/service/api/usage.js

---
+++ service/api/usage.js
@@ -0,0 +1,16 @@
+var formatter = require("../shared/usageMessageFormatter");
+
+exports.post = function(request, response) {
+    // Use "request.service" to access features of your mobile service, e.g.:
+    //   var tables = request.service.tables;
+    //   var push = request.service.push;
+
+    response.send(statusCodes.OK, { message : 'Hello World!' });
+};
+
+exports.get = function(request, response) {
+    // response.send(statusCodes.OK, { message : 'Hello World!' });
+    response.send({
+        message: formatter.format("ACTION", "EMAIL", "PASSWORD")
+    });
+};
