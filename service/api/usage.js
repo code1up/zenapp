16cb55f99e685c5d6bb572de00a1f41c2bac5c27
windowsazure <windowsazure>
2013-12-31 18:22:24 +0000

    Committing update from request /scmvfs/service/api/usage.js

--- service/api/usage.js
+++ service/api/usage.js
@@ -4,7 +4,9 @@ exports.get = function(request, response) {
     var email = request.headers["x-zen-email"];
     var password = request.headers["x-zen-password"];
     
+    var message = formatter.format("Authenticate", email, password);
+    
     response.send(statusCodes.OK, {
-        message: formatter.format("ACTION", "EMAIL", "PASSWORD")
+        message: message
     });
 };
