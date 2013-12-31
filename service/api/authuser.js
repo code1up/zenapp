d34e2462f97b3a3d44e9229f576f13a699eddab1
windowsazure <windowsazure>
2013-12-31 22:03:48 +0000

    Committing update from request /scmvfs/service/api/authuser.js

--- service/api/authuser.js
+++ service/api/authuser.js
@@ -1,7 +1,3 @@
 exports.post = function(request, response) {
     response.send(statusCodes.OK, { message : 'Hello World!' });
 };
-
-exports.get = function(request, response) {
-    response.send(statusCodes.OK, { message : 'Hello World!' });
-};
