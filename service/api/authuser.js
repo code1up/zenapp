107b1e2abe9cb2d172eb77baaf7911550583c42f
Alan Gorton <dev.code1up@googlemail.com>
2013-12-31 18:24:58 +0000

    wip

--- service/api/authuser.js
+++ service/api/authuser.js
@@ -1,8 +1,4 @@
 exports.post = function(request, response) {
-    // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
-    //   var push = request.service.push;
-
     response.send(statusCodes.OK, { message : 'Hello World!' });
 };
 
