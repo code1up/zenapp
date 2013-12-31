48198bc730310883d2eaa92d5f2fb52ab7b40932
windowsazure <windowsazure>
2013-12-31 18:19:57 +0000

    Committing update from request /scmvfs/service/api/usage.js

--- service/api/usage.js
+++ service/api/usage.js
@@ -1,10 +1,8 @@
 var formatter = require("../shared/usageMessageFormatter");
 
 exports.get = function(request, response) {
-    // response.send({ message : 'Hello World!' });
-    
-    console.log("Email: %s", request.headers["x-zen-email"]);
-    console.log("Password: %s", request.headers["x-zen-password"]);
+    var email = request.headers["x-zen-email"];
+    var password = request.headers["x-zen-password"];
     
     response.send(statusCodes.OK, {
         message: formatter.format("ACTION", "EMAIL", "PASSWORD")
