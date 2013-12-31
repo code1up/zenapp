835debe5f683b061e55ea6c35ac9c0a0b6835580
windowsazure <windowsazure>
2013-12-31 22:05:56 +0000

    Committing update from request /scmvfs/service/api/authuser.js

--- service/api/authuser.js
+++ service/api/authuser.js
@@ -1,3 +1,14 @@
-exports.post = function(request, response) {
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+var formatter = require("../shared/usageMessageformatter");
+var sender = require("../shared/usageMessagesender");
+
+exports.get = function(request, response) {
+    var email = request.headers["x-zen-email"];
+    var password = request.headers["x-zen-password"];
+    
+    var message = formatter.format("Authenticate", email, password);
+    var authentication = sender.send(message);
+    
+    response.send(statusCodes.OK, {
+        authentication: authentication
+    });
 };
