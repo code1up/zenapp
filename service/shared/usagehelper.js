807465f4b6e91b7e28504ef2e9d8342acb081dd6
Alan Gorton <dev.code1up@googlemail.com>
2014-01-05 10:43:08 +0000

    wip

--- service/shared/usagehelper.js
+++ service/shared/usagehelper.js
@@ -1,6 +1,6 @@
 var _ = require("underscore");
 
-export.resolve = function(obj, keys) {
+exports.resolve = function(obj, keys) {
     _.each(keys, function(key) {
         if (obj && obj.hasOwnProperty(key)) {
             obj = obj[key];
