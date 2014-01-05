32d1f0fcc94425e118e88dae23f40a4f20332578
Alan Gorton <dev.code1up@googlemail.com>
2014-01-05 10:25:07 +0000

    wip

--- service/api/authuser.js
+++ service/api/authuser.js
@@ -4,12 +4,11 @@ var parser = require("../shared/usagemessageparser");
 
 var _resolve = function(obj, keys) {
     _.each(keys, function(key) {
-        if (obj && obj.hasOwnProperty(key)) {
-            obj = obj[key];
-            continue;
+        if (!(obj && obj.hasOwnProperty(key)) {
+            return undefined;
         }
 
-        return undefined;
+        obj = obj[key];
     });
 
     return obj;
