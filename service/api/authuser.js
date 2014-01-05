a7fda66101e2244d10fa0cdeea8a9eec033e8150
Alan Gorton <dev.code1up@googlemail.com>
2014-01-05 11:41:52 +0000

    wip

--- service/api/authuser.js
+++ service/api/authuser.js
@@ -3,35 +3,36 @@ var usageActions = require("../shared/usageactions");
 var usageHelper = require("../shared/usagehelper");
 var usageMessage = require("../shared/usagemessage");
 
-var handler = function(error, soapResponse) {
-    if (error) {
-        var statusCode = error.statusCode || statusCodes.INTERNAL_SERVER_ERROR;
-
-        response.send(statusCode, error);
-        return;
-    }
-    
-    var token = usageHelper.resolve(soapResponse, [
-        "body", "AuthenticateResponse", "AuthenticateResult"
-    ]);
-
-    if (!token) {
-        response.send(statusCodes.INTERNAL_SERVER_ERROR, {
-            error: {
-                message: "Missing authentication token."
-            }
-        });
-
-        return;
-    }
-
-    response.send(statusCodes.OK, {
-        email: email,
-        token: token
-    });            
-};
-
 exports.get = function(request, response) {
+    
+    var handler = function(error, soapResponse) {
+        if (error) {
+            var statusCode = error.statusCode || statusCodes.INTERNAL_SERVER_ERROR;
+
+            response.send(statusCode, error);
+            return;
+        }
+        
+        var token = usageHelper.resolve(soapResponse, [
+            "body", "AuthenticateResponse", "AuthenticateResult"
+        ]);
+
+        if (!token) {
+            response.send(statusCodes.INTERNAL_SERVER_ERROR, {
+                error: {
+                    message: "Missing authentication token."
+                }
+            });
+
+            return;
+        }
+
+        response.send(statusCodes.OK, {
+            email: email,
+            token: token
+        });            
+    };
+
     credentialsParser.parse(request.headers, function(error, credentials) {
         if (error) {
             response.send(statusCodes.BAD_REQUEST, error);
