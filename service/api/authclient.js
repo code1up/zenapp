1a7fdb989a3d5d927e9ad5a7a2d865c473e72c99
windowsazure <windowsazure>
2014-06-16 11:38:59 +0000

    Committing update from request /scmvfs/service/api/authclient.js

--- service/api/authclient.js
+++ service/api/authclient.js
@@ -29,7 +29,7 @@ exports.get = function(request, response) {
         if (!clientAuthenticationToken) {
             response.send(statusCodes.INTERNAL_SERVER_ERROR, {
                 error: {
-                    message: "Missing client authentication token."
+                    message: "Missing client validation token."
                 }
             });
 
@@ -37,7 +37,7 @@ exports.get = function(request, response) {
         }
 
         response.send(statusCodes.OK, {
-            clientAuthenticationToken: clientAuthenticationToken
+            clientValidationToken: clientAuthenticationToken
         });            
     };
 
